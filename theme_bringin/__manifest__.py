b9b7858507ad2434540e87ba61308ad1cc35f34a
dombosm <dombos.miklos@oc.hu>
2022-03-25 16:49:48 +0100

    bringin template version 1

---
+++ theme_bringin/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Bring-In Theme',
+    'description': 'Theme for Hatter.hu',
+    'category': 'Theme/Corporate',
+    'summary': '',
+    'sequence': 110,
+    'version': '1.0.0',
+    'author': 'Dombos Miklós',
+    'depends': ['website'],
+    'data': [
+        'data/ir_asset.xml',
+        'views/images.xml',
+        'views/customizations.xml',
+    ],
+    'images': [
+        'static/description/bringin_poster.jpg',
+        'static/description/bringin_screenshot.png',
+    ],
+    'snippet_lists': {
+        'homepage': ['s_banner', 's_references', 's_text_image', 's_color_blocks_2', 's_images_wall'],
+    },
+    'license': 'LGPL-3',
+    'assets': {
+        'website.assets_editor': [
+            'theme_bringin/static/src/js/tour.js',
+        ],
+    }
+}
